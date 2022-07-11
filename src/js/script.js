9e2e5103d4e232b4b0a6051911ba191cf7911ccd
Elisa Andrade <elisalvsan@gmail.com>
2022-07-11 11:09:35 -0300

    finalização do script

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,18 @@
+//SELECIONANDO A LISTA PARA ADICIONAR OS PRODUTOS
 const produtosLista = document.querySelector(".produtosLista")
 const listaProdutos = document.querySelector(".listaProdutos")
 
+//0) CRIAR UM FUNÇÃO (CARDPRODUTO) => PROUTO COMO PARAMETRO
 function criarcardProduto(produto){
     
+    //1) TAG HTML  (LI)
     const tagLi  = document.createElement("li")
 
+    //2) ADICIONAR UMA CLASSE NA TAG LI (cardProduto)
     tagLi.classList.add("cardProduto")
     
+    //3) ALIMENTAR ESSA TAG COM OS OUTROS ELEMENTOS:
+    //4) ALIMENTAR COM INFORMAÇÕES DOS PRODUTOS
     tagLi.innerHTML = `
         <img src="${produto.image}" alt="${produto.nome}">
         <h3>${produto.nome}</h3>
@@ -18,12 +24,15 @@ function criarcardProduto(produto){
 
 function listarProdutos(listaProdutos){
 
+    //3) LOOP PARA RECUPERAR CADA PRODUTO
     for(let i  = 0; i<listaProdutos.length; i++){
         
         const produto = listaProdutos[i]
 
+        //4) PASSAR PARA FUNÇÃO/PARA CRIAR O CARD DE PRODUTO
         const cardMontado = criarcardProduto(produto)
        
+        //5) ADICIONAR ELEMENTO (LI) => NA TELA => COMO FILHO DA TAG UL
         produtosLista.appendChild(cardMontado)
 
     }
@@ -31,8 +40,10 @@ function listarProdutos(listaProdutos){
 }
 listarProdutos(produtos)
 
+//CRRINHO DE COMPRAS  
 let carrinho = []
 
+//ADICIONANDO INTERCEPTADOR NA LISTA DE PRODUTOS 
 produtosLista.addEventListener("click", adicionarProdutoCarrinho)
 function adicionarProdutoCarrinho(event){
     
@@ -42,24 +53,33 @@ function adicionarProdutoCarrinho(event){
     //VERIFICANDO SE É UM BOTÃO
     if(botao.tagName == "BUTTON"){
         
+        //RECUPERANDO ID => IDENTIFICADO DO PRODUTO
         const idProduto = botao.id
 
+        //PESQUISAR SE ESSE PRODUTO É EXISTENTE => BASE
         const produtoFiltrado = produtos.find((produto)=> produto.id == idProduto)
         
+        //ADICIONANDO PRODUTO NO CARRINHO
         carrinho.push(produtoFiltrado)
         
+        //LISTAR NA TELA DOS PRODUTOS
         listarProdutosCarrinho()
 
+        //FUNÇÕES QUE ATUALIZA TOTAL
+        atualizarTotal()
     }
     
 }
 
+//LISTAR PRODUTOS DO CARRINHO
 function listarProdutosCarrinho(){
 
     listaProdutos.innerHTML = ""
 
+    //PERCORRENDO PRODUTOS DO CARRINHO
     for(let i = 0; i<carrinho.length; i++){
 
+        //RECUPERANDO CADA PRODUTO
         const produto = carrinho[i]
 
         //CRIAR O TEMPLATE 
@@ -83,4 +103,31 @@ function listarProdutosCarrinho(){
         
     }
   
+}
+
+function atualizarTotal(){
+
+    const infoPreco = document.querySelector(".infoPreco")
+   
+    let total = 0
+    for(let i = 0; i < carrinho.length; i++){
+
+        const produto = carrinho[i]
+
+        total += produto.preco
+    }
+    infoPreco.innerText = `Total: R$ ${total.toFixed(2)}`
+
+}
+
+const listaProdutosCarrinho = document.querySelector(".listaProdutos")
+listaProdutosCarrinho.addEventListener("click", removerProdutoCarrinho)
+
+function removerProdutoCarrinho(event){
+    const botaoExcluir  = event.target
+
+    if(botaoExcluir.tagName == "BUTTON"){
+
+        botaoExcluir.closest("li").remove()
+    }
 }
